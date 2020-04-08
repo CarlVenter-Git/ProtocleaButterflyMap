140660902ed4f6213deac2f6ffda12ac9c72eef5
CarlVenter-Git <carlventer@live.co.za>
2020-04-08 22:57:25 +0200

    Sorted out some git issues, repo should be a bit cleaner now. Refactored all the code that imported the data from excel, it should not behave as expected and cause less crashes

--- ProtocleaButterflyMap.pas
+++ ProtocleaButterflyMap.pas
@@ -7,7 +7,8 @@ uses
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
   FMX.Controls.Presentation, FMX.StdCtrls, ComObj, FMX.Objects, IdBaseComponent,
   IdComponent, IdTCPConnection, IdTCPClient, IdHTTP, IdIOHandler,
-  IdIOHandlerSocket, IdIOHandlerStack, IdSSL, IdSSLOpenSSL, FMX.ListBox, DateUtils;
+  IdIOHandlerSocket, IdIOHandlerStack, IdSSL, IdSSLOpenSSL, FMX.ListBox, DateUtils,
+  Generics.Collections;
 
 type
   TForm1 = class(TForm)
@@ -52,7 +53,7 @@ end;
 
 var
   Form1: TForm1;
-  sightings: array of sightingRecord;//Not a fan of globals, but I'm not sure how to abstract effectively yet
+  sightings: TList<sightingRecord>;//Not a fan of globals, but I'm not sure how to abstract effectively yet
 
 implementation
 {$R *.fmx}
@@ -60,13 +61,17 @@ implementation
 procedure TForm1.btnLoadClick(Sender: TObject);
 var
   excel, book, sheet, range: OLEVariant;
-  selectedFilePath: string;
-  provinces: array of string;
-  years, months: array of TDateTime;
-  yearFound, monthFound, provinceFound: boolean;
-  x, y, i, j, numRows(*, numColumns*): integer;
+  recordToAdd: sightingRecord;
+  selectedFilePath, province: string;
+  provinces: TList<string>;
+  years, months: TList<integer>;
+  x, numRows, year, month(*, numColumns*): integer;
 
 begin
+  sightings := TList<sightingRecord>.Create;
+  provinces := TList<string>.Create;
+  years := TList<integer>.Create;
+  months := TList<integer>.Create;
 
   try
     if OpenDialog1.Execute then
@@ -76,6 +81,7 @@ begin
   end;
 
   if selectedFilePath <> '' then
+
   begin
 
     try
@@ -90,76 +96,47 @@ begin
       numRows := range.Rows.Count;
       //numColumns := range.Columns.Count; unused in this case
 
-      //Ignore the first line to make array correct size
-      SetLength(sightings, numRows - 1);
-      Setlength(years, 50); //I am making an assumption here, this would cause issues if we are looking at more than 50 years of data
-      SetLength(months, 12);
-      SetLength(provinces, 9); //I am more comfortable making this assumption
-
       for x := 2 to numRows do //Start at index 2 to skip the heading
       begin
         lblPath.Text := 'Loading ' + IntToStr(x - 1) + '/' + IntToStr(numRows);
-        y := x - 2;//So that the array index starts at 0
 
         //genus,species and subspecies is rather redundant in this case but still useful to have in the record for use at a later stage if the data ever includes more
-        sightings[y].rec_nr := sheet.Cells.Item[x, 1];
-        sightings[y].genus := sheet.Cells.Item[x, 2];
-        sightings[y].species := sheet.Cells.Item[x, 3];
-        sightings[y].subspecies := sheet.Cells.Item[x, 4];
-        sightings[y].rec_date := sheet.Cells.Item[x, 5];
-        sightings[y].province := sheet.Cells.Item[x, 6];
-        sightings[y].latitude := sheet.Cells.Item[x, 7];
-        sightings[y].longitude := sheet.Cells.Item[x, 8];
+        recordToAdd.rec_nr := sheet.Cells.Item[x, 1];
+        recordToAdd.genus := sheet.Cells.Item[x, 2];
+        recordToAdd.species := sheet.Cells.Item[x, 3];
+        recordToAdd.subspecies := sheet.Cells.Item[x, 4];
+        recordToAdd.rec_date := sheet.Cells.Item[x, 5];
+        recordToAdd.province := sheet.Cells.Item[x, 6];
+        recordToAdd.latitude := sheet.Cells.Item[x, 7];
+        recordToAdd.longitude := sheet.Cells.Item[x, 8];
 
-        yearFound := false;
-        monthFound := false;
-        provinceFound := false;
+        if not provinces.Contains(recordToAdd.province) then
+          provinces.Add(recordToAdd.province);
 
-        //This needs to be extracted, why is there not a Contains method for arrays??
-        for i := Low(years) to High(years) do
-        begin
-          if DateUtils.YearOf(years[i]) = DateUtils.YearOf(sightings[y].rec_date) then
-          begin
-            yearFound := true;
-          end;
-        end;
+        if not months.Contains(StrToInt(Dateutils.MonthOf(recordToAdd.rec_date).ToString)) then
+          months.Add(StrToInt(DateUtils.MonthOf(recordToAdd.rec_date).ToString));
 
-        for i := Low(months) to High(months) do
-        begin
-          if DateUtils.MonthOf(months[i]) = DateUtils.MonthOf(sightings[y].rec_date) then
-          begin
-            monthFound := true;
-          end;
-        end;
-
-        for i := Low(provinces) to High(provinces) do
-        begin
-          if provinces[i] = sightings[y].province then
-          begin
-            provinceFound := true;
-          end;
-        end;
+        if not years.Contains(StrToInt(DateUtils.YearOf(recordToAdd.rec_date).ToString)) then
+          years.Add(StrToInt(DateUtils.YearOf(recordToAdd.rec_date).ToString));
 
 
-        if yearFound = false then
-        begin
-          cmbYear.Items.Add(DateUtils.YearOf(sightings[y].rec_date).ToString);
-        end;
-
-        if monthFound = false then
-        begin
-          cmbMonth.Items.Add(DateUtils.MonthOf(sightings[y].rec_date).ToString);
-        end;
-
-        if provinceFound = false then
-        begin
-          provinces[i] := sightings[y].province;
-          cmbProvince.Items.Add(sightings[y].province);
-        end;
-
+        sightings.Add(recordToAdd);
       end;
 
+      years.Sort;
+      months.Sort;
+      provinces.Sort;
+
       lblPath.Text := selectedFilePath + ' Successfully Loaded';
+
+      for year in years do
+        cmbYear.Items.Add(IntToStr(year));
+
+      for month in months do
+        cmbMonth.Items.Add(IntToStr(month));
+
+      for province in provinces do
+        cmbProvince.Items.Add(province);
 
     finally
       excel.Quit;
@@ -185,7 +162,8 @@ begin
   y := Panel1.Size.Height;
   markerString := '';
 
-  for i := Low(sightings) to High(sightings) do
+  for i := 0 to sightings.Count do
+
   begin
     if (sightings[i].province = cmbProvince.Selected.Text) and
     (DateUtils.YearOf(sightings[i].rec_date).ToString = cmbYear.Selected.Text) and
